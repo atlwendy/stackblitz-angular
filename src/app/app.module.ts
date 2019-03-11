35597ff2d1a465358b6384d373265ba25bc926de
Wendy Yang <atlwendy@gmail.com>
2019-03-11 16:58:13 -0400

    try out material input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
 
+import {
+  MatInputModule,
+} from '@angular/material';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
+
 export class AppModule { }
